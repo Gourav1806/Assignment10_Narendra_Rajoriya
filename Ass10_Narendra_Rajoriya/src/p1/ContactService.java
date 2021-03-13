531af79c95267c39a2479fe313a32d776174140e
NARENDRA <NARENDRA@DESKTOP-QGA1LRQ>
2021-03-13 13:33:30 +0530

    First Commit created class contact and contactservice

---
+++ Ass10_Narendra_Rajoriya/src/p1/ContactService.java
@@ -0,0 +1,38 @@
+package p1;
+import java.io.*;
+import java.util.*;
+class Contact{
+	private int  contactId;
+	private String contactName;
+	private String email;
+	private List<String> contactNumber;
+	public int getContactId() {
+		return contactId;
+	}
+	public void setContactId(int contactId) {
+		this.contactId = contactId;
+	}
+	public String getContactName() {
+		return contactName;
+	}
+	public void setContactName(String contactName) {
+		this.contactName = contactName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public List<String> getContactNumber() {
+		return contactNumber;
+	}
+	public void setContactNumber(List<String> contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+	
+	
+}
+public class ContactService {
+
+}
